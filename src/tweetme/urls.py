9c2a3459900da491fba600bb975697059f53c838
parkchansoo <chansoo1563@gmail.com>
2018-01-29 10:05:18 +0900

    basic crud with django

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -19,11 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import home
+from django.conf.urls import include
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/$', home, name='home')
+    url(r'^$', home, name='home'),
+    url(r'^tweets/', include('tweets.urls')),
 ]
 
 if settings.DEBUG:
